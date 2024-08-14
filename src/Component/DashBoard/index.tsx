d3ae257a85461442c27be599b9e3b5355487798a
James DivineMercy <iamoluchimercy@gmail.com>
2024-08-14 03:24:32 +0100

    new commit

---
+++ src/Component/DashBoard/index.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+
+const DashBoard : React.FC = () => {
+    return (
+        <div className={"text-6xl"}>
+            Dashboard
+        </div>
+    )
+}
+
+export default DashBoard
---
+++ src/Route/Route.tsx
@@ -0,0 +1,8 @@
+import DashBoard from "../Component/DashBoard";
+
+export const Route = [
+    {
+        path : "/",
+        element : <DashBoard/>
+    },
+]
